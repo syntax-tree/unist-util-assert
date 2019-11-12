fa8d7eb785e78ba537f5d0a1e5edbfe6f7098aaa
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-11-12 14:52:30 -0700

    types: upgrade dtslint to support ts 3.7, support void assertion

--- types/unist-util-assert-test.ts
+++ types/unist-util-assert-test.ts
@@ -1,9 +1,25 @@
-import {Node, Parent, Literal} from 'unist'
-import unified = require('unified')
-import assert = require('unist-util-assert')
+import * as assert from 'unist-util-assert'
 
 function testAssert() {
   const node = {}
   assert(node)
-  node
+  node // $ExpectType Node
+}
+
+function testParentAssert() {
+  const node = {}
+  assert.parent(node)
+  node // $ExpectType Parent
+}
+
+function testTextAssert() {
+  const node = {}
+  assert.text(node)
+  node // $ExpectType Literal
+}
+
+function testVoidAssert() {
+  const node = {}
+  assert.void(node)
+  node // $ExpectType VoidNode
 }
